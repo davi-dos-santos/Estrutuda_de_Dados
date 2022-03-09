3f516a2ec18e6c816c1487cdb2af88364991edaf
davi1701 <davi.spereira1@gmial.com>
2022-03-08 22:06:26 -0300

    teste de .py

--- Estutura_dados/Funcoes.py
+++ Estutura_dados/Funcoes.py
@@ -3,10 +3,16 @@ def perguntar():
                "(I) Inserir um usuario \n" +
                "(P) Pesquisar um usuario \n" +
                "(E) Excluir um usaurio \n" +
-               "(L) Listar um usario \n \n").upper()
+               "(L) Listar um usario \n").upper()
     return resposta
 
 def inserir(dicinonario):
     dicinonario[input("Digite o Login: ").upper() ] = [input("digite o nome: ").upper(),
-                                                       input("difite a ultima data de acesso"),
+                                                       input("difite a ultima data de acesso: "),
                                                        input("difite a ultima estaçao acessada: ").upper()]
+def pesquisar(dicinario, chave):
+    lista = dicinario.get (chave)
+    if lista!=None:
+        print("Nome............: \n"+ lista[0])
+        print("Ultimo accesso..: \n" + lista[1])
+        print("Ultima estaçaõ..: \n" + lista[2])
