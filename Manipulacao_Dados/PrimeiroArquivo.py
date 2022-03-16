db03d800828978812e99fc8e2810d2a881581a18
davi1701 <davi.spereira1@gmial.com>
2022-03-16 00:58:27 -0300

    atvidade yp

--- Manipulacao_Dados/PrimeiroArquivo.py
+++ Manipulacao_Dados/PrimeiroArquivo.py
@@ -1,3 +1,16 @@
+# # w = criar aquivo, podendo tambem reescrever em cima do já criado
+# with open("primeiro_arquivo.txt", "w") as arquivo:
+#     arquivo.write("\n primeira linha do arquivo py")
+#
+# # a = edita arquivo, aplicando escrita em linhas
+# with open("primeiro_arquivo.txt", "a") as arquivo:
+#     arquivo.write("\n segunda linha do arquivo py")
+#
+# # r = leitura do arquivo, imprime as informações salvas
+# with open("primeiro_arquivo.txt", "r") as arquivo:
+#     conteudo = arquivo.read()
+#     print(conteudo)
 
-with open("primeiro_arquivo.txt", "a") as arquivo:
-    arquivo.write("\n segunda linha do arquivo py")
+with open("primeiro_arquivo.txt", "r") as arquivo:
+    for linha in arquivo.readlines():
+        print(linha)
