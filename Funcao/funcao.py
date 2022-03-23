4a8e334052a4537a2d6cf25a14c1bed0ad8aa7a0
davi1701 <davi.spereira1@gmial.com>
2022-03-22 21:05:01 -0300

    atvidade de funções py

---
+++ Funcao/funcao.py
@@ -0,0 +1,40 @@
+# def nome da funcao (parametro1, parametros2, ...)
+# corpo da funcao
+
+# def foo (n):
+#     return n
+#
+# print(foo(10))
+
+# def foo (n):
+#     print("o valor do paraametro é: ", n)
+#
+# def elevado_ao_quadrado(n):
+#     return n ** 2
+#
+# print(elevado_ao_quadrado(5))
+
+# def foo (n):
+#     print("o valor do paraametro é: ", n)
+#
+# def elevado_ao_quadrado(n):
+#     return n ** 2
+#
+# def potencia(n, pot):
+#     return n ** pot
+#
+# print(potencia(2, 10))
+
+def foo (n):
+    print("o valor do paraametro é: ", n)
+
+def elevado_ao_quadrado(n):
+    return n ** 2
+
+def potencia(n, pot):
+    return n ** pot
+
+def eh_positivo(n):
+    return n >= 0
+
+print(eh_positivo(10))
